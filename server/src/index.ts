417eae5056f9b61ec5c3e9f7957d095cba1b7d09
Aritra640 <chatterjeearitra15@gmail.com>
2025-10-21 23:43:39 +0530

    fix: fixed turbo build

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import type { ApiResponse } from "shared/dist";
 
 export const app = new Hono()
 
@@ -10,13 +9,5 @@ export const app = new Hono()
 	return c.text("Hello Hono!");
 })
 
-.get("/hello", async (c) => {
-	const data: ApiResponse = {
-		message: "Hello BHVR!",
-		success: true,
-	};
-
-	return c.json(data, { status: 200 });
-});
 
 export default app;
