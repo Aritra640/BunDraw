ea5f969ae724d0fe3920b7791413a61c686724eb
Aritra640 <chatterjeearitra15@gmail.com>
2025-11-06 03:11:41 +0530

    minor changes

--- server/src/auth/authmiddleware.ts
+++ server/src/auth/authmiddleware.ts
@@ -1,3 +1,28 @@
-function authmiddleware() {}
+import { createMiddleware } from "hono/factory";
+import { verifyJwt } from "./jwt";
+
+const authmiddleware = createMiddleware(async (c, next) => {
+  console.log("authentication middleware has been called");
+
+  const authHeader = c.req.header("Authentication");
+  if (!authHeader) {
+    return c.json({ error: "Missing Authentication header" }, 404);
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return c.json({ error: "Missing Authentication token" }, 404);
+  }
+
+  try {
+    const username = await verifyJwt(token);
+    c.set("username", username);
+
+    await next();
+  } catch (err) {
+    console.log("JWT verification has failed: ", err);
+    return c.json({ error: "Authentication failed" }, 401);
+  }
+});
 
 export default authmiddleware;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,15 +1,18 @@
+import authmiddleware from "@server/auth/authmiddleware";
 import { Hono } from "hono";
 
 const user = new Hono().basePath("/user");
 
+user.use("/auth/*", authmiddleware);
+
 user.post("/signup", (c) => c.text("signup user"));
 user.post("/signin", (c) => c.text("signin user"));
 
-user.delete("/signout", (c) => c.text("log out user"));
-user.delete("/delete/:id", (c) => c.text("delete user"));
+user.delete("/auth/signout", (c) => c.text("log out user"));
+user.delete("/auth/delete/:id", (c) => c.text("delete user"));
 
 user.put("/forgetpassword/:email", (c) => c.text("forget user password"));
-user.put("/username", (c) => c.text("change username"));
-user.put("/password", (c) => c.text("change password"));
+user.put("/auth/username", (c) => c.text("change username"));
+user.put("/auth/password", (c) => c.text("change password"));
 
 export default user;
