167df29c17f99eaf6a1add1f0f37fa9808384c32
Aritra640 <chatterjeearitra15@gmail.com>
2025-11-12 01:54:05 +0530

    initial commit

--- server/src/dbutils/user.ts
+++ server/src/dbutils/user.ts
@@ -1,38 +1,103 @@
-export async function GetUserHashedPassword(username: string): Promise<string> {
-  return "asb";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function GetUserHashedPassword(
+  username: string,
+): Promise<string | null> {
+  const user = await prisma.user.findUnique({
+    where: { name: username },
+    select: { password: true },
+  });
+
+  return user?.password ?? null;
 }
 
 interface Userdata {
-  Username: string;
-  Email: string;
-  HashedPassword: string;
-  AvatarUrl?: string;
+  success: boolean;
+  message: string;
+  user?: {
+    Id: number;
+    Username: string;
+    Email: string;
+    AvatarUrl?: string;
+  };
 }
 
-export async function GetUserFromUsername(
-  username: string,
-): Promise<[Userdata, boolean]> {
-  return [
-    {
-      Username: "TestUser",
-      Email: "testmail.mail.com",
-      HashedPassword: "test hashedpassword",
+export async function GetUserFromUsername(username: string): Promise<Userdata> {
+  const user = await prisma.user.findUnique({
+    where: { name: username },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      avatar: true,
     },
-    true,
-  ];
+  });
+
+  if (!user) {
+    console.log("User with username: ", username, " was not found!");
+    return {
+      success: false,
+      message: "userdata not found in the database",
+    };
+  }
+
+  return {
+    success: true,
+    message: "userdata was found in the database",
+    user,
+  };
 }
 
-export async function GetUserFromEmail(
-  email: string,
-): Promise<[Userdata, boolean]> {
-  return [
-    {
-      Username: "TestUser",
-      Email: "testmail.mail.com",
-      HashedPassword: "test hashedpassword",
+export async function GetUserFromEmail(email: string): Promise<Userdata> {
+  const user = await prisma.user.findUnique({
+    where: { email: email },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      avatar: true,
     },
-    true,
-  ];
+  });
+
+  if (!user) {
+    console.log("User with email: ", email, " was not found in the database!");
+    return {
+      success: false,
+      message: "userdata was not found in the database",
+    };
+  }
+
+  return {
+    success: true,
+    message: "userdata was found in the database",
+    user,
+  };
+}
+
+export async function GetUserFromId(id: number): Promise<Userdata> {
+  const user = await prisma.user.findUnique({
+    where: { id: id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      avatar: true,
+    },
+  });
+  if (!user) {
+    console.log("User with id: ", id, " was not found in the database!");
+    return {
+      success: false,
+      message: "userdata was not found in the database",
+    };
+  }
+
+  return {
+    success: true,
+    message: "userdata was found in the database",
+    user,
+  };
 }
 
 export type NewUser = {
@@ -42,4 +107,134 @@ export type NewUser = {
   Avatar?: string;
 };
 
-export async function AddNewUser(payload: NewUser) {}
+export type AddNewUserResult = {
+  success: boolean;
+  message: string;
+  user?: {
+    id: number;
+    name: string;
+    email: string;
+    avatar?: string;
+  };
+};
+
+export async function AddNewUser(payload: NewUser): Promise<AddNewUserResult> {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: payload.Username,
+        email: payload.Email,
+        password: payload.Password,
+        avatar: payload.Avatar,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        avatar: true,
+      },
+    });
+
+    console.log("User has been sucessfully added!");
+
+    return {
+      success: true,
+      message: "User added successfully!",
+      user,
+    };
+  } catch (error) {
+    console.log("An error occured while adding new user: ", error);
+    return {
+      success: false,
+      message: "something went wrong!",
+    };
+  }
+}
+
+export async function DeleteUserFromId(id: number): Promise<boolean> {
+  const deleteUser = await prisma.user.delete({
+    where: { id: id },
+  });
+
+  if (!deleteUser) {
+    return false;
+  }
+  return true;
+}
+
+export async function DeleteUserFromUsername(
+  username: string,
+): Promise<boolean> {
+  const deleteUser = await prisma.user.delete({
+    where: { name: username },
+  });
+
+  if (!deleteUser) {
+    return false;
+  }
+
+  return true;
+}
+
+export async function DeleteUserFromEmail(email: string): Promise<boolean> {
+  const deleteUser = await prisma.user.delete({
+    where: { email: email },
+  });
+
+  if (!deleteUser) {
+    return false;
+  }
+
+  return true;
+}
+
+export async function UpdateUsernameById(
+  id: number,
+  username: string,
+): Promise<boolean> {
+  const updatedUser = await prisma.user.update({
+    where: { id: id },
+    data: {
+      name: username,
+    },
+  });
+
+  if (!updatedUser) {
+    return false;
+  }
+  return true;
+}
+
+export async function UpdateUsernameByEmail(
+  email: string,
+  username: string,
+): Promise<boolean> {
+  const updatedUser = await prisma.user.update({
+    where: { email: email },
+    data: {
+      name: username,
+    },
+  });
+
+  if (!updatedUser) {
+    return false;
+  }
+  return true;
+}
+
+export async function UpdateUserPasswordByEmail(
+  email: string,
+  password: string,
+): Promise<boolean> {
+  const updatedUser = await prisma.user.update({
+    where: { email: email },
+    data: {
+      password: password,
+    },
+  });
+
+  if (!updatedUser) {
+    return false;
+  }
+  return true;
+}
